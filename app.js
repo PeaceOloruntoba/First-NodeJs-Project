e772b02cb2b9668e596f1f365b44cce100b822f8
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-05-23 22:10:41 +0000

    mod

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 5517;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World!');
 });
 
 server.listen(port, hostname, () => {
